974e741e1733a01c719c5bfb400f96259fa55c01
Harshit Singh <singhh198@gmail.com>
2024-06-27 20:58:25 +0530

    Commiting first version of project

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,20 @@
 */
 
 function isAnagram(str1, str2) {
-
+  sz1 = str1.length;
+  sz2 = str2.length;
+  if(sz1!=sz2)return false;
+  flg = true;
+  for (var i = 0; i < sz1; i++) {
+    if (str1[i] != str2[sz1 - 1 - i]) {
+      flg = false;
+      break;
+    }
+  }
+  if (flg) return true;
+  else return false;
 }
 
 module.exports = isAnagram;
+
+// console.log(isAnagram('ab','ba')) - To print in vsCode
